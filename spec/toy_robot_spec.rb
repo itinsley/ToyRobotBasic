c4b6ec37079a51eb77dbcc6245c01aebb6482744
Ian Tinsley <itinsley@gmail.com>
2016-08-09 23:05:51 +1000

    Add invalid position error validation

--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe "ToyRobot" do
   specify 'that ToyRobot f is set to EAST' do
     expect(subject.f).to eq(:east)
   end
+
+
+  describe '#place' do
+   specify 'When x is less than zero it should raise an exception' do
+      expect { subject.place(-1,0) }.to raise_error(InvalidPositionError, "Invalid Position, -1, 0")
+    end
+   specify 'When y is less than zero it should raise an exception' do
+      expect { subject.place(0,-1) }.to raise_error(InvalidPositionError, "Invalid Position, 0, -1")
+    end
+  end
+
 end
--- toy_robot.rb
+++ toy_robot.rb
@@ -1,3 +1,5 @@
+require_relative "invalid_position_error"
+
 class ToyRobot
   attr_reader :x, :y, :f, :movements
 
@@ -7,6 +9,9 @@ class ToyRobot
     @f = :east
   end
 
+  def place(x,y)
+    raise InvalidPositionError.new("Invalid Position, #{x}, #{y}") if (x<0 || y<0)
+  end
 end
 
 # robot = ToyRobot.new
